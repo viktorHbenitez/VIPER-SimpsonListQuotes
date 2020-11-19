8b7c9dff439c177f95cc98339f8236278445f1ba
VICTOR HUGO BENITEZ BOSQUES <vbenitez@bancoazteca.com>
2020-11-18 18:41:35 -0600

    Add project Simpson List

---
+++ VIPER-ListSimpsonQuotes/VIPER-ListSimpsonQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER-ListSimpsonQuotes
+//
+//  Created by Victor Hugo Benitez Bosques on 18/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
